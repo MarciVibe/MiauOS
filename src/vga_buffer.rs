f3e7f96f38044362a2abe3c8c512811a2f9d7367
MarciVibe <roen0014@asms.sa.edu.au>
2023-06-29 10:20:11 +0930

    Formatted Macros

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -94,7 +94,16 @@ impl Writer {
     }
 }
 
+use core::fmt;
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+ }
 pub fn print_something() {
+    use core::fmt::Write;
     let mut writer = Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
@@ -102,6 +111,6 @@ pub fn print_something() {
     };
 
     writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("Wörld!");
+    writer.write_string("ello evry nyyan ");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
